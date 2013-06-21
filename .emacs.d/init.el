aadb781161ea75650856db726124754edbc3f955
Correl Roush <croush@coredial.com>
2013-06-21 12:41:21 -0400

    [emacs] Override font-lock-faces to avoid changing the font family or size

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -125,10 +125,12 @@
  '(indent-tabs-mode nil)
  '(inferior-lisp-program "sbcl")
  '(inhibit-startup-screen t))
+(put 'narrow-to-region 'disabled nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
-(put 'narrow-to-region 'disabled nil)
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(font-lock-function-name-face ((t (:foreground "LightSkyBlue" :weight bold))))
+ '(font-lock-type-face ((t (:foreground "PaleGreen" :weight bold)))))
