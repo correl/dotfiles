8303454123302a2538591e8f867ce0188a20596d
Correl Roush <croush@coredial.com>
2013-08-07 13:18:32 -0400

    [emacs] Disable git-gutter when running ediff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,6 +29,15 @@
 ;; Git Gutter
 (global-git-gutter-mode t)
 
+(defadvice ediff-make-temp-file (before make-temp-file-suspend-ll
+                                        activate compile preactivate)
+  "Disable git-gutter when running ediff"
+  (global-git-gutter-mode 0))
+
+(add-hook 'ediff-cleanup-hook
+          '(lambda ()
+             (global-git-gutter-mode t)))
+
 ;; Fixes an issue with ECB
 ;; (setq stack-trace-on-error t)
 
