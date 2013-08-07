925f957405a25279b024410588043fb13effd33c
Correl Roush <croush@coredial.com>
2013-08-07 18:36:55 -0400

    [emacs] Set diff-mode colors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -41,6 +41,17 @@
 ;; Fixes an issue with ECB
 ;; (setq stack-trace-on-error t)
 
+;; Diff colors
+(defun custom-diff-colors ()
+  "update the colors for diff faces"
+  (set-face-attribute
+   'diff-added nil :foreground "green")
+  (set-face-attribute
+   'diff-removed nil :foreground "red")
+  (set-face-attribute
+   'diff-changed nil :foreground "purple"))
+(eval-after-load "diff-mode" '(custom-diff-colors))
+
 ;; SLIME
 (if (file-exists-p "~/quicklisp/slime-helper.el")
     (load (expand-file-name "~/quicklisp/slime-helper.el")))
