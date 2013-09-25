2d34efccf0bc267537107e786c62f6028c5937b6
Correl Roush <correl@gmail.com>
2013-09-24 21:34:30 -0400

    [emacs] Customize PHP indentation

--- .emacs.d/init.d/php.el
+++ .emacs.d/init.d/php.el
@@ -2,6 +2,14 @@
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 
+(defun my-php-mode-hook ()
+  "Customize PHP indentation"
+
+  (c-set-offset 'arglist-cont-nonempty 'c-lineup-arglist)
+  (c-set-offset 'substatement-open 0))
+
+(add-hook 'php-mode-hook 'my-php-mode-hook)
+
 ;; Multi-Web-Mode
 (require 'multi-web-mode)
 (setq mweb-default-major-mode 'html-mode)
