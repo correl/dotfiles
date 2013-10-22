7c3f2951e0e7ae67348ddeb0e5348fc972fc697f
Correl Roush <croush@coredial.com>
2013-10-22 11:09:04 -0400

    [emacs] include smarty templates in multi-web mode

--- .emacs.d/init.d/php.el
+++ .emacs.d/init.d/php.el
@@ -17,5 +17,5 @@
 (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
                   (js-mode "<script[^>]*>" "</script>")
                   (css-mode "<style[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5" "tpl"))
 (multi-web-global-mode 1)
