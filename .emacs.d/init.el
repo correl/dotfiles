7cf6340438a2662f7ccfda7a40e0f7df30505ed6
Correl Roush <croush@coredial.com>
2014-05-14 08:11:44 -0400

    [emacs] Update ELPA url

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,8 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("elpa" . "http://tromey.com/elpa/"))
+             '("elpa" . "http://elpa.gnu.org/packages/"))
+
 ;; Add the user-contributed repository
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
