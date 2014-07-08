906d0c8c5890797813f0952d38d11236943a5fa5
Correl Roush <croush@coredial.com>
2014-07-08 13:51:16 -0400

    [emacs] Replace multi-web-mode with web-mode

--- .emacs.d/init.d/php.el
+++ .emacs.d/init.d/php.el
@@ -10,12 +10,3 @@
   (c-set-offset 'case-label '+))
 
 (add-hook 'php-mode-hook 'my-php-mode-hook)
-
-;; Multi-Web-Mode
-(require 'multi-web-mode)
-(setq mweb-default-major-mode 'html-mode)
-(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-                  (js-mode "<script[^>]*>" "</script>")
-                  (css-mode "<style[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5" "tpl"))
-(multi-web-global-mode 1)
