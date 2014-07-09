3d36eb4462c959a174cfeb9b014215900a98c0ca
Correl Roush <croush@coredial.com>
2014-07-09 09:43:16 -0400

    [emacs] web-mode configuration

---
+++ .emacs.d/init.d/web-mode.el
@@ -0,0 +1,4 @@
+(add-hook 'web-mode-hook (lambda ()
+                           (setq web-mode-markup-indent-offset 4)
+                           (setq web-mode-css-indent-offset 4)
+                           (setq web-mode-code-indent-offset 4)))
