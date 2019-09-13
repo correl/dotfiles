14b670a564da746ee48ee817b3f461708f3fe49f
Correl Roush <correl@gmail.com>
2019-09-13 10:54:23 -0400

    [emacs] Add personal org-mode config

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -5,3 +5,5 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
+
+(package! ox-confluence-en :recipe (:host github :repo "correl/ox-confluence-en"))
