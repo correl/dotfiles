992b03e8ac58f1ce5583b9289510a134a9a271c7
Correl Roush <correl@gmail.com>
2020-01-24 13:22:06 -0500

    [emacs] Add my kanji and owo overlay modes

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -6,7 +6,10 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
+(package! kanji-mode)
+(package! kanji-glasses-mode :recipe (:host github :repo "correl/kanji-glasses-mode"))
 (package! kerl :recipe (:host github :repo "correl/kerl.el"))
+(package! owo-mode :recipe (:host github :repo "correl/owo-mode"))
 (package! ox-confluence-en :recipe (:host github :repo "correl/ox-confluence-en"))
 (package! jira-api :recipe (:host github :repo "correl/jira-api"))
 (package! ob-http)
