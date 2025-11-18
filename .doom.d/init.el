28a8359b9ddaadea141ec44ef0bf5ee87b81f09e
Correl Roush <correl@gmail.com>
2025-11-18 16:22:19 -0500

    [emacs] Disable eglot and pyright
    lsp-mode is more featureful, and I'd rather pyright didn't take
    precedence over ruff.

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -100,7 +100,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        lookup              ; navigate your code and its documentation
        ;;llm               ; when I said you needed friends, I didn't mean...
-       (lsp +eglot)      ; M-x vscode
+       lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        pass              ; password manager for nerds
@@ -177,7 +177,6 @@
         +lsp
         +poetry
         +pyenv
-        +pyright
         +tree-sitter
         )
        ;;qt                ; the 'cutest' gui framework ever
